f3ebb527b14be6c0f7094f525f73ecf623a5a033
David Knight <dknight@weblinc.com>
2013-01-22 00:51:31 -0500

    Updated code base to only support native viewport units

--- css.vunits.js
+++ css.vunits.js
@@ -1,88 +1,42 @@
 /* CSSVUnits - Viewport relative units. Authors & copyright (c) 2013: WebLinc, David Knight. */
+/* NOTE: Only supports native implementation of viewport units. This forces repaint so units resize as viewport changes. */
 
-// CSSVUnit
+// CSSVUnits (Native viewport units resize)
 (function(win, undefined) {
     'use strict';
 
     // Internal globals
-    var _doc            = win.document,
-        _style          = null,
-        _styleSheet     = null,
-        _cssRules       = null,
-        _length         = 0,
-        _width          = 0,
-        _height         = 0,
-        _timer          = 0,
+    var _doc        = win.document,
+        _style      = null,
+        _styleSheet = null,
+        _cssRules   = null,
+        _length     = 0,
+        _timer      = 0,
 
-        _getViewport    = function () {
-            _width      = win.innerWidth || _doc.documentElement.clientWidth;
-            _height     = win.innerHeight || _doc.documentElement.clientHeight;
-        },
-        
-        _insertRules    = function (options) {
-            var style   = null,
-                cssText = '';
+        _repaint    = function (selector) {
+            var index = _cssRules.length;
 
-            if (!options || !(options.style) || (options.media && !options.media.matches)) {
-                return;
-            }
-
-            for (var prop in options.style) {
-                if (options.style.hasOwnProperty(prop)) {
-                    var item    = options.style[prop],
-                        value   = item.replace(/(\d+)(vw|vh|vmin|vmax)/gi, function (p1, p2, p3) {
-                            var multiplier  =   (p3 === 'vw' && _width) || 
-                                                (p3 === 'vh' && _height) || 
-                                                (p3 === 'vmin' && (_width < _height ? _width  : _height)) || 
-                                                (p3 === 'vmax' && (_width < _height ? _height : _width));
-
-                            return ((p2 * multiplier) / 100).toFixed(2) + 'px';
-                        });
-
-                    cssText += prop + ': ' + value + ';';
-                }
-            }
-
-            if (typeof options.cssRuleIndex === 'undefined') {
-                options.cssRuleIndex = _cssRules.length;
-
-                if (_styleSheet.insertRule) {
-                    _styleSheet.insertRule(options.selector + '{}', options.cssRuleIndex);
-                } else {
-                    _styleSheet.addRule(options.selector, ' ', options.cssRuleIndex);
-                }
-            }
-
-            style = _cssRules[options.cssRuleIndex].style;
-            cssText && style.cssText !== cssText && (style.cssText = cssText);
+            _styleSheet.insertRule(selector + '{ z-index: 1; }', index);
+            _styleSheet.deleteRule(index);
         };
 
     // CSSVUnits
     win.CSSVUnits = {
         styleSheet : [],
 
-        add     : function (options) {
-            if (!options) {
+        add     : function (selector) {
+            if (!selector) {
                 return -1;
             }
 
-            _getViewport();
-
-            if (options.media) {
-                options.media = (win.matchMedia && win.matchMedia(options.media)) || {
-                    matches : true,
-                    media   : options.media
-                };
-            }
-
-            _length = this.styleSheet.push(options);
-            _insertRules(options);
+            _length = this.styleSheet.push(selector);
+            _repaint(selector);
 
             return _length - 1;
         },
 
         remove  : function (index) {
-            typeof index !== 'undefined' && this.styleSheet.splice(index, 1);
+            index >= 0 && this.styleSheet.splice(index, 1);
         },
 
         watch   : function () {
@@ -96,10 +50,8 @@
                     return;
                 }
 
-                _getViewport();
-
                 do {
-                    _insertRules(win.CSSVUnits.styleSheet[il - i]);
+                    _repaint(win.CSSVUnits.styleSheet[il - i]);
                 } while (i--);
             }, 10);
         },
@@ -111,16 +63,22 @@
 
             _doc.getElementsByTagName('head')[0].appendChild(_style);
 
-            _styleSheet = _style.sheet || _style.styleSheet;
-            _cssRules   = _styleSheet.cssRules || _styleSheet.rules;
-
-            if (win.addEventListener) {
-                win.addEventListener('resize', this.watch);
-                win.addEventListener('orientationchange', this.watch);
-            } else {
-                win.attachEvent('onresize', this.watch);
-                win.attachEvent('onorientationchange', this.watch);
+            if (typeof _style.textContent === 'undefined' && !_style.sheet && !_style.sheet.cssRules) {
+                _style.parentNode.removeChild(_style);
+                return;
             }
+
+            _style.textContent  = '#CSSVUnit { width: 1vw; height: 1vh; }';
+            _styleSheet = _style.sheet;
+            _cssRules   = _styleSheet.cssRules;
+
+            if (!_cssRules[0].style.width || !_cssRules[0].style.height) {
+                _style.parentNode.removeChild(_style);
+                return;
+            }
+
+            win.addEventListener('resize', this.watch);
+            win.addEventListener('orientationchange', this.watch);
         }
     };
 
