2eb5f9993bc250504450d429d6351c8dcd447007
David Knight <dknight@weblinc.com>
2013-01-28 23:44:33 -0500

    Added support for 1.1vw property values. Updated support for media queries. Add basic comments. Added more to the test.html example page.

--- css.vunits.js
+++ css.vunits.js
@@ -14,35 +14,46 @@
         _height         = 0,
         _timer          = 0,
 
+        /*
+            _getViewport
+         */
         _getViewport    = function () {
             _width      = win.innerWidth || _doc.documentElement.clientWidth;
             _height     = win.innerHeight || _doc.documentElement.clientHeight;
         },
-        
+
+        /*
+            _insertRules
+         */
         _insertRules    = function (options) {
             var style   = null,
                 cssText = '';
 
-            if (!options || !(options.style) || (options.media && !options.media.matches)) {
+            // If options or style is undefined, stop
+            if (!options || !options.style) {
                 return;
             }
 
-            for (var prop in options.style) {
-                if (options.style.hasOwnProperty(prop)) {
-                    var item    = options.style[prop],
-                        value   = item.replace(/(\d+)(vw|vh|vmin|vmax)/gi, function (p1, p2, p3) {
-                            var multiplier  =   (p3 === 'vw' && _width) || 
-                                                (p3 === 'vh' && _height) || 
-                                                (p3 === 'vmin' && (_width < _height ? _width  : _height)) || 
-                                                (p3 === 'vmax' && (_width < _height ? _height : _width));
+            // If media is undefined or media matches, loop through styles and build cssText
+            if (!options.media || options.media.matches) {
+                for (var prop in options.style) {
+                    if (options.style.hasOwnProperty(prop)) {
+                        var item    = options.style[prop],
+                            value   = item.replace(/(\d+.?\d*)(vw|vh|vmin|vmax)/gi, function (p1, p2, p3) {
+                                var multiplier  =   (p3 === 'vw' && _width) || 
+                                                    (p3 === 'vh' && _height) || 
+                                                    (p3 === 'vmin' && (_width < _height ? _width  : _height)) || 
+                                                    (p3 === 'vmax' && (_width < _height ? _height : _width));
 
-                            return ((p2 * multiplier) / 100).toFixed(2) + 'px';
-                        });
+                                return ((p2 * multiplier) / 100).toFixed(2) + 'px';
+                            });
 
-                    cssText += prop + ': ' + value + ';';
+                        cssText += prop + ': ' + value + ';';
+                    }
                 }
             }
 
+            // If options does not have 'cssRuleIndex', create the property then insert rules at that index
             if (typeof options.cssRuleIndex === 'undefined') {
                 options.cssRuleIndex = _cssRules.length;
 
@@ -53,14 +64,19 @@
                 }
             }
 
+            // Use 'cssRuleIndex' to access previously created rule then set cssText
             style = _cssRules[options.cssRuleIndex].style;
-            cssText && style.cssText !== cssText && (style.cssText = cssText);
+            style.cssText !== cssText && (style.cssText = cssText);
         };
 
     // CSSVUnits
     win.CSSVUnits = {
+        // Contains selector, style and/or media objects
         styleSheet : [],
 
+        /*
+            add
+         */
         add     : function (options) {
             if (!options) {
                 return -1;
@@ -81,10 +97,26 @@
             return _length - 1;
         },
 
+        /*
+            remove
+         */
         remove  : function (index) {
-            typeof index !== 'undefined' && this.styleSheet.splice(index, 1);
+            if (this.styleSheet[index]) {
+                 var cssRuleIndex = this.styleSheet[index].cssRuleIndex;
+
+                 if (_styleSheet.deleteRule) {
+                     _styleSheet.deleteRule(cssRuleIndex);
+                 } else {
+                     _styleSheet.removeRule(cssRuleIndex);
+                 }
+
+                 this.styleSheet.splice(index, 1);
+            }
         },
 
+        /*
+            watch
+         */
         watch   : function () {
             clearTimeout(_timer);
 
@@ -104,7 +136,14 @@
             }, 10);
         },
 
+        /*
+            init
+         */
         init    : function () {
+            if (_style) {
+                return;
+            }
+
             _style              = _doc.createElement('style')
             _style.type         = 'text/css';
             _style.id           = 'CSSVUnit';
